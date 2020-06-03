fcf34771868b8d11afa3c43093fb036eb2316fc0
zhangyanling <18783226594@163.com>
2020-06-03 21:35:35 +0800

    refator: update config

--- .umirc.ts
+++ .umirc.ts
@@ -6,7 +6,9 @@ export default defineConfig({
     'https://avatars1.githubusercontent.com/u/19699453?s=400&u=f2a9905fd0bc7573e2feb227cef17e6d0c42e185&v=4',
   description: '基于react的UI组件库',
   theme: {
-    '@c-primary': 'orange',
+    '@c-primary': 'magenta',
   },
+  favicon:
+    'https://avatars1.githubusercontent.com/u/19699453?s=400&u=f2a9905fd0bc7573e2feb227cef17e6d0c42e185&v=4',
   // more config: https://d.umijs.org/config
 });
