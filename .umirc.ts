a526f9f2f925201ace106ac5c5ec9d6bad11a6a9
zhangyanling <18783226594@163.com>
2020-06-03 21:13:39 +0800

    feat：通过脚手架create @umijs/dumi-lib初始化

---
+++ .umirc.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: 'zyl-view',
+  logo:
+    'https://avatars1.githubusercontent.com/u/19699453?s=400&u=f2a9905fd0bc7573e2feb227cef17e6d0c42e185&v=4',
+  description: '基于react的UI组件库',
+  theme: {
+    '@c-primary': 'orange',
+  },
+  // more config: https://d.umijs.org/config
+});
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# zyl-view
-基于react的UI组件库，脚手架为@umijs/dumi-app，文档使用dumi
+# dumi library
+
+## Getting Started
+
+安装依赖
+
+```bash
+$ npm i
+```
+
+启动 dev server,
+
+```bash
+$ npm start
+```
+
+组件文档构建
+
+```bash
+$ npm run docs:build
+```
+
+由 `father-build` 构建组件库
+
+```bash
+$ npm run build
+```
